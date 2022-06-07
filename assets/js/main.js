1a3a312e041b17aeeefb4d25df95e2bd792a3631
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2022-06-08 01:43:13 +0600

    3 pages added

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,9 +89,30 @@
         }
       }
     });
+  });
 
-
-
+  $('.testimonial-wrapper-2').owlCarousel({
+    autoplay: true,
+    margin: 15,
+    loop: true,
+    infinite: true,
+    nav: true,
+    dots: false,
+    items: 1,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
   });
 
   /*---------------------
@@ -153,6 +174,22 @@
     }
 
   });
+
+  /*---------------------
+    Elivated Zoom
+  --------------------- */
+  $(".elivate-lg-img").elevateZoom({
+    gallery: 'thumb-img',
+    zoomType: "inner",
+    cursor: "crosshair",
+  });
+
+
+  $(document).ready(function () {
+    $('.cv-carousel').carouselVertical().trigger('goTo', [5]);
+  });
+
+
 
   /*---------------------
   Price Range Dynamic
@@ -238,6 +275,7 @@
   });
 
 
+
   /*---------------------
    Contact Form
   ---------------------*/
@@ -287,5 +325,201 @@
     });
 
   }
+
+  /*---------------------
+  Product Details Page 
+  --------------------- */
+  $(window).on('load', function () {
+    $('.xzoom, .xzoom-gallery').xzoom({
+      zoomWidth: 400,
+      title: true,
+      tint: '#333',
+      Xoffset: 15
+    });
+    $('.xzoom2, .xzoom-gallery2').xzoom({
+      position: '#xzoom2-id',
+      tint: '#ffa200'
+    });
+    $('.xzoom3, .xzoom-gallery3').xzoom({
+      position: 'lens',
+      lensShape: 'circle',
+      sourceClass: 'xzoom-hidden'
+    });
+    $('.xzoom4, .xzoom-gallery4').xzoom({
+      tint: '#006699',
+      Xoffset: 15
+    });
+    $('.xzoom5, .xzoom-gallery5').xzoom({
+      tint: '#006699',
+      Xoffset: 15
+    });
+
+    //Integration with hammer.js
+    var isTouchSupported = 'ontouchstart' in window;
+
+    if (isTouchSupported) {
+      //If touch device
+      $('.xzoom, .xzoom2, .xzoom3, .xzoom4, .xzoom5').each(function () {
+        var xzoom = $(this).data('xzoom');
+        xzoom.eventunbind();
+      });
+
+      $('.xzoom, .xzoom2, .xzoom3').each(function () {
+        var xzoom = $(this).data('xzoom');
+        $(this).hammer().on("tap", function (event) {
+          event.pageX = event.gesture.center.pageX;
+          event.pageY = event.gesture.center.pageY;
+          var s = 1,
+            ls;
+
+          xzoom.eventmove = function (element) {
+            element.hammer().on('drag', function (event) {
+              event.pageX = event.gesture.center.pageX;
+              event.pageY = event.gesture.center.pageY;
+              xzoom.movezoom(event);
+              event.gesture.preventDefault();
+            });
+          }
+
+          xzoom.eventleave = function (element) {
+            element.hammer().on('tap', function (event) {
+              xzoom.closezoom();
+            });
+          }
+          xzoom.openzoom(event);
+        });
+      });
+
+      $('.xzoom4').each(function () {
+        var xzoom = $(this).data('xzoom');
+        $(this).hammer().on("tap", function (event) {
+          event.pageX = event.gesture.center.pageX;
+          event.pageY = event.gesture.center.pageY;
+          var s = 1,
+            ls;
+
+          xzoom.eventmove = function (element) {
+            element.hammer().on('drag', function (event) {
+              event.pageX = event.gesture.center.pageX;
+              event.pageY = event.gesture.center.pageY;
+              xzoom.movezoom(event);
+              event.gesture.preventDefault();
+            });
+          }
+
+          var counter = 0;
+          xzoom.eventclick = function (element) {
+            element.hammer().on('tap', function () {
+              counter++;
+              if (counter == 1) setTimeout(openfancy, 300);
+              event.gesture.preventDefault();
+            });
+          }
+
+          function openfancy() {
+            if (counter == 2) {
+              xzoom.closezoom();
+              $.fancybox.open(xzoom.gallery().cgallery);
+            } else {
+              xzoom.closezoom();
+            }
+            counter = 0;
+          }
+          xzoom.openzoom(event);
+        });
+      });
+
+      $('.xzoom5').each(function () {
+        var xzoom = $(this).data('xzoom');
+        $(this).hammer().on("tap", function (event) {
+          event.pageX = event.gesture.center.pageX;
+          event.pageY = event.gesture.center.pageY;
+          var s = 1,
+            ls;
+
+          xzoom.eventmove = function (element) {
+            element.hammer().on('drag', function (event) {
+              event.pageX = event.gesture.center.pageX;
+              event.pageY = event.gesture.center.pageY;
+              xzoom.movezoom(event);
+              event.gesture.preventDefault();
+            });
+          }
+
+          var counter = 0;
+          xzoom.eventclick = function (element) {
+            element.hammer().on('tap', function () {
+              counter++;
+              if (counter == 1) setTimeout(openmagnific, 300);
+              event.gesture.preventDefault();
+            });
+          }
+
+          function openmagnific() {
+            if (counter == 2) {
+              xzoom.closezoom();
+              var gallery = xzoom.gallery().cgallery;
+              var i, images = new Array();
+              for (i in gallery) {
+                images[i] = {
+                  src: gallery[i]
+                };
+              }
+              $.magnificPopup.open({
+                items: images,
+                type: 'image',
+                gallery: {
+                  enabled: true
+                }
+              });
+            } else {
+              xzoom.closezoom();
+            }
+            counter = 0;
+          }
+          xzoom.openzoom(event);
+        });
+      });
+
+    } else {
+      //If not touch device
+
+      //Integration with fancybox plugin
+      $('#xzoom-fancy').bind('click', function (event) {
+        var xzoom = $(this).data('xzoom');
+        xzoom.closezoom();
+        $.fancybox.open(xzoom.gallery().cgallery, {
+          padding: 0,
+          helpers: {
+            overlay: {
+              locked: false
+            }
+          }
+        });
+        event.preventDefault();
+      });
+
+      //Integration with magnific popup plugin
+      $('#xzoom-magnific').bind('click', function (event) {
+        var xzoom = $(this).data('xzoom');
+        xzoom.closezoom();
+        var gallery = xzoom.gallery().cgallery;
+        var i, images = new Array();
+        for (i in gallery) {
+          images[i] = {
+            src: gallery[i]
+          };
+        }
+        $.magnificPopup.open({
+          items: images,
+          type: 'image',
+          gallery: {
+            enabled: true
+          }
+        });
+        event.preventDefault();
+      });
+    }
+  });
 
 }(jQuery));
